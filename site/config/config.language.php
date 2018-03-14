20e05a6b94d669914572d9ee5798a447fe5d7b37
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-03-14 08:56:59 +0100

    Enable multi languages in config (Dutch and English).

--- site/config/config.language.php
+++ site/config/config.language.php
@@ -27,27 +27,27 @@ More information about language codes, locales and multi-language sitemaps:
 */
 
 c::set('locale', 'en_US.UTF8');                                                 // Sets the global locale setting for PHP (e.g. format a local time/date according to locale settings)
-c::set('language.multi', false);                                                // Enable multi-language support (also make sure to out-comment the `languages` variabel array below!)
+c::set('language.multi', true);                                                 // Enable multi-language support (also make sure to out-comment the `languages` variabel array below!)
 c::set('language.locale', 'en');                                                // Variable to set (among other things) the lang atribute on the html element when multi-language is disabled
 c::set('language.detect', false);                                               // Enables automatic language detection for multi-language sites
 
-// c::set('languages', array(
-// 	array(
-// 		'default'   => true,
-// 		'code'      => 'en',
-// 		'name'      => 'English',
-// 		'locale'    => 'en',                                                    // English content
-// 		// 'locale'    => 'en_US',                                                 // English content, for US users
-// 		// 'locale'    => 'en_GB',                                                 // English content, for United Kingdom users
-// 		'url'       => '/',
-// 		'direction' => 'ltr'
-// 	),
-// 	array(
-// 		'code'      => 'nl',
-// 		'name'      => 'Nederlands',
-// 		'locale'    => 'nl_NL',                                                 // Dutch content
-// 		// 'locale'    => 'nl_BE'                                                  // Dutch content, for Belgium user
-// 		'url'       => '/nl',
-// 		'direction' => 'ltr'
-// 	),
-// ));
+c::set('languages', array(
+	array(
+		'code'      => 'en',
+		'name'      => 'English',
+		'locale'    => 'en',                                                    // English content
+		// 'locale'    => 'en_US',                                                 // English content, for US users
+		// 'locale'    => 'en_GB',                                                 // English content, for United Kingdom users
+		'url'       => '/en',
+		'direction' => 'ltr'
+	),
+	array(
+		'default'   => true,
+		'code'      => 'nl',
+		'name'      => 'Nederlands',
+		'locale'    => 'nl_NL',                                                 // Dutch content
+		// 'locale'    => 'nl_BE'                                                  // Dutch content, for Belgium user
+		'url'       => '/',
+		'direction' => 'ltr'
+	),
+));
