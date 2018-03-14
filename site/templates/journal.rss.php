88ee23211a016018156af0cc1d9e47548804b061
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-03-14 10:13:32 +0100

    Use description fields for dagboek rss feed text.
    Using the text field doesn’t yet work; a combination for imagekit/set plugins and multi language.

--- site/templates/journal.rss.php
+++ site/templates/journal.rss.php
@@ -5,11 +5,11 @@
 /////////////////////////////////////////////////////////////
 
 // echo page('blog')->children()->visible()->flip()->limit(20)->feed(array(
-echo page('journal')->children()->visible()->flip()->feed(array(
-	'channel'       => 'journal',
-	'textfield'     => 'text',
-	'image'         => false,                                                   // Include image (after excerpt); first available image in folder is used (set to false when images are included in 'kirbytext' field, and excerpt is set to false, to prevent duplicate images!)
-	'excerpt'       => false,
+echo page('dagboek')->children()->visible()->flip()->feed(array(
+	'channel'       => 'dagboek',
+	'textfield'     => 'description',
+	'image'         => true,                                                   // Include image (after excerpt); first available image in folder is used (set to false when images are included in 'kirbytext' field, and excerpt is set to false, to prevent duplicate images!)
+	'excerpt'       => true,
 	'excerptlimit'  => 'words',                                                 // Limit excerpts by the number of 'words' (default) or 'characters'
 	'excerptlenght' => 40,                                                      // Excerpt lenght in words or characters (depends on `excerptlimit` setting)
 ));
