c6f34d35349d83bbdc114553abf1a9f9ae6b6eea
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-04-01 19:21:24 +0200

    Add interface language translations for Dutch and English.

--- site/languages/en.php
+++ site/languages/en.php
@@ -3,22 +3,57 @@
 // Default
 l::set('submit', 'Submit');
 l::set('cancel', 'Cancel');
+l::set('close', 'Close');
 
+// HTML Head
 l::set('title', 'Title');
 l::set('description', 'Description');
+
+// Languages
+l::set('translations', 'Translations of this page');
+l::set('change language', 'Change language');
+l::set('change language to', 'Taal veranderen naar');
+
+// Navigation
+l::set('main navigation', 'Main navigation');
+l::set('page navigation', 'Page navigation');
 
 // Pagination
 l::set('next', 'Next page');
 l::set('prev', 'Previous page');
 l::set('page', 'page');
 
+// SVG
+l::set('left arrow title', 'Left arrow');
+l::set('left arrow desc', 'An arrow pointing to the left');
+l::set('right arrow title', 'Right arrow');
+l::set('right arrow desc', 'An arrow pointing to the right');
+l::set('cross title', 'Cross');
+l::set('cross desc', 'A cross or close icon');
+
 // Journal
+l::set('journal posts', 'Journal posts');
 l::set('posted', 'Posted');
 l::set('posted in', 'Posted in journal');
 l::set('posted on', 'Posted on');
 l::set('on', 'on');
 l::set('recent posts', 'Recent journal posts');
 l::set('more posts', 'More journal posts');
+
+// Work
+l::set('work', 'Work');
+l::set('view work', 'View work');
+l::set('close overlay', 'Close overlay');
+
+// Painting
+l::set('material and technique', 'Material &amp; Technique');
+l::set('measurements', 'Measurements');
+l::set('year of painting', 'Year');
+
+// Filters
+l::set('filter work', 'Filter work');
+l::set('remove filter', 'Remove filter');
+l::set('tagged with', 'series');
 
 // Time & Dates
 l::set('no date', 'No (correct) date field defined in content file!');
--- site/languages/nl.php
+++ site/languages/nl.php
@@ -7,18 +7,51 @@ l::set('cancel', 'Annuleer');
 l::set('title', 'Titel');
 l::set('description', 'Beschrijving');
 
+// Languages
+l::set('translations', 'Vertalingen van deze pagina');
+l::set('change language', 'Verander taal');
+l::set('change language to', 'Change language to');
+
+// Navigation
+l::set('main navigation', 'Hoofd navigatie');
+l::set('page navigation', 'Pagina navigatie');
+
 // Pagination
 l::set('next', 'Volgende pagina');
 l::set('prev', 'Vorige pagina');
 l::set('page', 'pagina');
 
+// SVG
+l::set('left arrow title', 'Pijl links');
+l::set('left arrow desc', 'Een pijl naar links wijzend');
+l::set('right arrow title', 'Pijl rechts');
+l::set('right arrow desc', 'Een pijl naar rechts wijzend');
+l::set('cross title', 'Kruis');
+l::set('cross desc', 'Een kruis of sluit pictogram');
+
 // Journal
+l::set('journal posts', 'Dagboek-items');
 l::set('posted', 'Geplaatst');
 l::set('posted in', 'Geplaatst in dagboek');
 l::set('posted on', 'Geplaatst op');
 l::set('on', 'op');
-l::set('recent posts', 'Recente dagboekitems');
-l::set('more posts', 'Meer dagboek items');
+l::set('recent posts', 'Recente dagboek-items');
+l::set('more posts', 'Meer dagboek-items');
+
+// Work
+l::set('work', 'Werk');
+l::set('view work', 'Bekijk werk');
+l::set('close overlay', 'Sluit laag');
+
+// Painting
+l::set('material and technique', 'Materiaal &amp; Techniek');
+l::set('measurements', 'Afmetingen');
+l::set('year of painting', 'Jaar');
+
+// Filters
+l::set('filter work', 'Filter werk');
+l::set('remove filter', 'Verwijder filter');
+l::set('tagged with', 'serie');
 
 // Dates
 l::set('no date', '[Geen (correcte) datum gedefinieerd in het tekstbestand]');
