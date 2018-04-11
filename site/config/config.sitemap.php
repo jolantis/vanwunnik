ac362dfe1edc6d0113503d5f6cee1c6d49913fac
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-04-11 12:36:05 +0200

    Add image license (url) to sitemap config, and show frequency.

--- site/config/config.sitemap.php
+++ site/config/config.sitemap.php
@@ -10,10 +10,10 @@ https://github.com/pedroborges/kirby-xml-sitemap
 
 */
 
-c::set('sitemap.images.license', null);                                         // License URL for your site's images
+c::set('sitemap.images.license', 'https://creativecommons.org/licenses/by-nc-nd/4.0/'); // License URL for your site's images
 c::set('sitemap.include.images', true);                                         // Enable/disable adding images to XML Sitemap
 c::set('sitemap.include.invisible', false);                                     // Add/remove invisible pages
-c::set('sitemap.frequency', false);                                             // Show/hide change frequency attribute
+c::set('sitemap.frequency', true);                                              // Show/hide change frequency attribute
 c::set('sitemap.priority', true);                                               // Show/hide priority attribute
 c::set('sitemap.ignored.pages', []);                                            // URI of pages to remove
 c::set('sitemap.ignored.templates', []);                                        // Templates names to remove
