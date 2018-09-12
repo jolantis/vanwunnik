5d06a78271af90baf6fe1c6dab499b3122099333
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-09-12 14:01:58 +0200

    Update FontFaceObserver code.
    Use what's described on the official FontFaceObserver page. Also add (out-commented) examples of timeout and prioritised loading.

--- assets/javascript/modules/fontobserver.module.js
+++ assets/javascript/modules/fontobserver.module.js
@@ -1,57 +1,84 @@
 /**
  * Fontobserver module
+ *
+ * More info: https://www.bramstein.com/writing/web-font-loading-patterns.html
  */
 
 var FontObserverHandler = (function () {
 
-	function init(){
+	function init() {
 
 		// If the class `fonts-loaded` is already set, we're good
-		if(document.documentElement.className.indexOf('fonts-loaded') > -1 ) {
+		if (document.documentElement.className.indexOf('fonts-loaded') > -1) {
+			// if (document.documentElement.classList.contains('fonts-loaded')) {
 			return;
 		}
 
 		// Define the fonts and font variants to observed
-		var fontObservers = [];
-		var fontFamilies = {
-			'Rosario': [
-				{
-					weight: 400
-				},
-				{
-					weight: 400,
-					style: 'italic'
-				},
-				{
-					weight: 700
-				},
-				{
-					weight: 700,
-					style: 'italic'
-				},
-			],
-		};
-
-		// Init the observer on the defined font settings
-		Object.keys(fontFamilies).forEach(function(family) {
-			fontObservers.push(fontFamilies[family].map(function(config) {
-				return new FontFaceObserver(family, config).load();
-			}));
+		var rosario_regular = new FontFaceObserver('Rosario', {
+			weight: 400
+		});
+		var rosario_italic = new FontFaceObserver('Rosario', {
+			weight: 400,
+			style: 'italic'
+		});
+		var rosario_bold = new FontFaceObserver('Rosario', {
+			weight: 700
+		});
+		var rosario_bold_italic = new FontFaceObserver('Rosario', {
+			weight: 700,
+			style: 'italic'
 		});
 
-		// Set the `fonts-loaded` class and avoid re-downloading the web fonts
-		// over and over again (and do not run the fontobserver script when the
-		// font is already available from cache) by setting a cookie
-		Promise
-			.all(fontObservers)
-			.then(function() {
-				// document.documentElement.classList.add('fonts-loaded');
-				document.documentElement.className += ' fonts-loaded';
-				enhance.cookie('fonts_loaded', 'true', 7);
-				// cookie.set('fonts_loaded', 'true', 7);
-			}, function() {
-				console.info('Web fonts could not be loaded in time. Falling back to system fonts.');
-			});
+		// Loading groups of fonts with a timeout
+		Promise.all([
+			rosario_regular.load(),
+			rosario_italic.load(),
+			rosario_bold.load(),
+			rosario_bold_italic.load()
+		]).then(function () {
+			document.documentElement.className += ' fonts-loaded';
+			// document.documentElement.classList.add('fonts-loaded');
+			enhance.cookie('fonts_loaded', 'true', 7);
+			// console.log('Kawak fonts have loaded.');
+		}).catch(function () {
+			// document.documentElement.classList.add('fonts-failed');
+			console.info('Web fonts could not be loaded in time. Falling back to system fonts.');
+		});
+
+		// // Timer helper function
+		// function timer(time) {
+		// 	return new Promise(function (resolve, reject) {
+		// 		setTimeout(reject, time);
+		// 	});
+		// }
+
+		// // Loading groups of fonts with a timeout
+		// Promise.race([
+		// 	timer(3000),
+		// 	kawak_light.load(),
+		// 	kawak_regular.load()
+		// ]).then(function () {
+		// 	document.documentElement.className += ' fonts-loaded';
+		// 	// document.documentElement.classList.add('fonts-loaded');
+		// 	enhance.cookie('fonts_loaded', 'true', 7);
+		// 	// console.info('Kawak fonts have loaded.');
+		// }).catch(function () {
+		// 	// document.documentElement.classList.add('fonts-failed');
+		// 	console.info('Kawak fonts loading has timed out (> 3 sec.). Falling back to system fonts.');
+		// });
+
+		// // Prioritised loading
+		// kawak_light.load().then(function () {
+		// 	document.documentElement.className += ' kawak-light-loaded';
+		// 	console.info('Kawak Light font has loaded.');
+
+		// 	kawak_regular.load().then(function () {
+		// 		document.documentElement.className += ' kawak-regular-loaded';
+		// 		document.documentElement.className += ' fonts-loaded';
+		// 		console.info('Kawak Regular font has loaded.');
+		// 	});
+		// });
 
 	}
 
